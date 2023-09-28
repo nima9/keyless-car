74e3e884b36807218c5ff69adc71c8bea7aadb62
Nima Mohajeri <nima.mohajeri0@gmail.com>
2023-09-28 00:49:30 -0700

    fix not showing blogs. and titles a group3 blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
