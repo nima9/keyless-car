f25b75a2ac19aa1b0ce0f474723cf86d62e3a728
Nima Mohajeri <nima.mohajeri0@gmail.com>
2023-09-28 00:02:23 -0700

    installed astro vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
